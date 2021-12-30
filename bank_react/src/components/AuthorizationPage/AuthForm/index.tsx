b84eb2ebdb3e4613aa702c578dec12d0dc39e42c
marchewazz <mateuszooospot@gmail.com>
2021-12-30 13:15:18 +0100

    Small fix to make code better

--- bank_react/src/components/AuthorizationPage/AuthForm/index.tsx
+++ bank_react/src/components/AuthorizationPage/AuthForm/index.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 
-import PINForm from "../../PINForm";
-
 import AuthService from "../../../services/AuthService";
 
 function AuthForm(){
@@ -27,6 +25,21 @@ function AuthForm(){
             }
         })
     }
+
+    function validatePIN(event: any){
+        event.preventDefault();
+        const data = new FormData(event.target);
+        const userData = {
+            accountEmail: email,
+            accountPIN: data.get("pin")
+        }
+        as.validatePINByEmail(userData).then((res: any) => {
+            if(res.data.message === "Logged!") {
+                window.location.href = `http://localhost:4200/confirmedauth/?accountnumber=${JSON.parse(res.data.user).accountNumber}`;
+            }
+        })
+    }
+
     return(
         <>
             <fieldset disabled={isLogged}>
@@ -37,7 +50,10 @@ function AuthForm(){
                 </form>
             </fieldset>
             {isLogged ?(
-                <PINForm email={email} />
+                <form onSubmit={validatePIN}>
+                    <input type="password" name="pin" minLength={4} maxLength={4} placeholder="Pass PIN" required />
+                    <button className="btn-style">OK</button>
+                </form>
             ) : (
                 <div></div>
             )}
