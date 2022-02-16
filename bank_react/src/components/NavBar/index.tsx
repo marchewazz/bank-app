3bc02bd11a2543244bfe05efec863ead764a9dc2
marchewazz <mateuszooospot@gmail.com>
2022-02-16 16:41:29 +0100

    Navbar styling

--- bank_react/src/components/NavBar/index.tsx
+++ bank_react/src/components/NavBar/index.tsx
@@ -20,18 +20,40 @@ export default function NavBar(){
     }, [as])
 
     return (
-        <div>
-            <Link to="/">Home</Link> |{" "}
+        <div className="w-screen h-12 grid grid-rows-1 grid-flow-col justify-items-stretch bg-red-800 text-white
+        divide-x-8 divide-white">
+            <span className="grid place-items-center text-lg">
+                Amazing Bank
+            </span>
+            <Link to="/"
+            className="navbar-link">
+                Home
+            </Link>
             { !isLogged ? (
                 <>
-                    <Link to="/register">Register</Link> |{" "}
-                    <Link to="/login">Login</Link>
+                    <Link to="/login"
+                    className="navbar-link">
+                        Login
+                    </Link>
+                    <Link to="/register"
+                    className="navbar-link">
+                        Register
+                    </Link>
                 </>
             ) : (
                 <>
-                    <Link to="/profile" className="underline">Hello!</Link> |{" "}
-                    <Link to="/transaction">Make transfer</Link> |{" "}
-                    <button onClick={logout}>Logout</button>
+                    <Link to="/transaction"
+                    className="navbar-link">
+                        Make transfer
+                    </Link>
+                    <Link to="/profile" 
+                    className="underline navbar-link">
+                        Your profile
+                    </Link> 
+                    <button onClick={logout}
+                    className="navbar-link">
+                        Logout
+                    </button>
                 </>
             )}
         </div>
